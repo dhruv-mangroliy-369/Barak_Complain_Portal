7bd4387c68a6390ac95f367c711c1bfc50175aa8
dhruv-mangroliy-369 <dhruvmangroliya@gmail.com>
2023-07-09 21:07:49 +0530

    Loading message changed

--- src/Home.js
+++ src/Home.js
@@ -15,8 +15,7 @@ const Home = () => {
             <div className="home">
 
                 {err && <div>{err}</div>}
-                {ispending && <div><h2><b>"Loading..."</b></h2></div>}
-                {ispending && <div><h2>"It may take 30 to 40 seconds to load."</h2></div>}
+                {ispending && <div><h2><b>"Loading..."</b></h2></div> &&<div><h4>"It may take 30 to 40 seconds to load."</h4></div>}
                 {!ispending && <>
                     <div className="card" >
                         <ul className="list-group list-group-flush">
